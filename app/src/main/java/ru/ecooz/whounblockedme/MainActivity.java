35e47fdbfd86cfe8c03018c8b7bf0cf1fca2d322
Рустам.Атаи <rystam.atai@gmail.com>
2017-07-04 12:26:35 +0300

    Initial commit

---
+++ app/src/main/java/ru/ecooz/whounblockedme/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.ecooz.whounblockedme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
